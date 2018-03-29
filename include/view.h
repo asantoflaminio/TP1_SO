4a287f42cc5c6e5e178f0dc6f482155667547a13
Alejandro <alejandro@localhost.localdomain>
2018-03-29 16:48:24 -0300

    Error de permission denied en view

---
+++ include/view.h
@@ -0,0 +1,12 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/wait.h>
+#include <sys/sem.h>
+#include <string.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define MYSIZE 10000 //no se por ahora le puse este size
+
+void modifySemaphore(int x,int idSem);
--- view.c
+++ view.c
@@ -1,16 +1,11 @@
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <string.h>
-#include <errno.h>
-#include <stdio.h>
+#include "include/view.h"
 
-#define MYSIZE 10000 //no se por ahora le puse este size
+
 
 int main(void){
-
+	
 	//ftok recibe path y nro como parametro
-	key_t key;
+	/*key_t key;
 	int id;
 	int idSem;
 	char* shm; //esta es mi memoria compartida. Va a ser un string de chars. 
@@ -18,56 +13,56 @@ int main(void){
 	int done = 0;
 	int cant=1;
 	int i=0;
-	int status=1;
+	int status=1;*/
 
-	key = ftok ("/home", 123); // uso un directorio que este en toda PC
+	/*key = ftok ("/home", 123); // uso un directorio que este en toda PC
 	if(key == -1){
 		perror("Error during key generation");
 		wait(NULL);
 		exit(1);
-	}
+	}*/
 
 	//creamos zona de memoria compartida. Hay que chequear que no sea -1.
-	id = shmget(key, MYSIZE, 0777 | IPC_CREAT);
+	/*id = shmget(key, MYSIZE, 0777 | IPC_CREAT);
 	if(id == -1){
 		perror("Error while creating shared memory");
 		wait(NULL);
 		exit(1);
-	}
+	}*/
 
 	//Le indicamos al sistema que tome dirección donde fijar el segmento.
-	shm = shmat(id, NULL, 0);
+	/*shm = shmat(id, NULL, 0);
 	if(shm == (char*) -1){
 		perror("Error while taking a segment");
 		wait(NULL);
 		exit(1);
-	}
+	}*/
 
 	//esto va a ser despues para el semaforo
-	idSem = semget (key, 1, 0777 | IPC_CREAT);
+	/*idSem = semget (key, 1, 0777 | IPC_CREAT);
 	if (idSem == -1)
 	{
 		printf("Error during semaphore creation\n");
 		wait(NULL);
 		exit (1);
-	}
+	}*/
 
 
 
 	//aca vendria la impresion de todos los hashes y el uso de semaforos
 
-	while(!done){
+	/*while(!done){
 		modifySemaphore(-1,idSem);
 		if(shm[i]!= EOF){
 			while(shm[i]!='!'){
-				printf("%c", i); //Suponiendo que un signo de exclamacion es con lo q separo los hashes o con lo que termino el mensaje de cada archivo
+				printf("%c", shm[i]); //Suponiendo que un signo de exclamacion es con lo q separo los hashes o con lo que termino el mensaje de cada archivo
 			}
-			printf('\n');
+			printf("\n");
 		}else{
 			done = 1;
 		}
 		modifySemaphore(1,idSem);
-	}
+	}*/
 	/* VERSION GIULO
 	while(!done) {
 		modifySemaphore(-1,idSem);
@@ -94,25 +89,25 @@ int main(void){
 		modifySemaphore(1,idSem);
 	}*/
 
-	modifySemaphore(-1,idSem);
-	memory[1]=-1;
+	/*modifySemaphore(-1,idSem);
+	//shm[1]=-1;
 	modifySemaphore(1,idSem);
 
-
+	
 	//cuando terminamos liberamos todo. Chequear igual si es necesario hacerlo. 
-	shmdt ((char *)s);
-	shmctl (id, IPC_RMID, (struct shmid_ds *)NULL);
+	shmdt ((char *)shm);
+	shmctl (id, IPC_RMID, (struct shmid_ds *)NULL);*/
 
-	printf("View has concluded");
+	printf("View has concluded\n");
 	return 0;
 
 }
 
 //Tal vez convendria moverlo a otro lado
-void modifySemaphore(int x,int idSem){
+/*void modifySemaphore(int x,int idSem){
 	struct sembuf operation;
 	operation.sem_num = 0;
 	operation.sem_op = x;
 	operation.sem_flg = 0;
 	semop(idSem, &operation, 1);
-}
+}*/
